62d215528bfd5f5d51207d6e8743b20e04b58cf0
leoderigo <leoderigo.sjb@gmail.com>
2026-09-17 13:18:22 -0300

    main funcionality and error handling implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let mut matched_lines: Vec<&'a str> = Vec::new();
+    for line in contents.lines() {
+        if !line.contains(query) { continue };
+        matched_lines.push(line);
+    }
+    matched_lines
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "pokemons";
+        let contents = "\
+There are no pokemons here.
+Nor here.
+Or even here.";
+
+        assert_eq!(vec!["There are no pokemons here."], search(query, contents));
+    }
 }
