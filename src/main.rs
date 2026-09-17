7089ee2f77be1d7491efd044f1b0417754e997d3
leoderigo <leoderigo.sjb@gmail.com>
2026-09-17 12:59:46 -0300

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
-use std::{env, fs};
+use std::{env, error, fs, process};
+use testing_the_docs::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let search_for = &args[1];
-    let pokemon = &args[2];
-    let filepath: String = format!("entries/{}.txt", pokemon);
+    let config= Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem building configuration file: {}", err);
+        process::exit(1);
+    });
+    
+    if let Err(err) = run(config) {
+        println!("Error during the execution of the application: {}", err);
+        process::exit(1);
+    };
+}
 
-    println!("Searching for \"{}\" in {} pokedex entry", search_for, pokemon);
+fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
+    println!("Searching for '\"{}\" in {} pokedex entry", config.query, config.pokemon);
+    println!("");
+    let contents = fs::read_to_string(config.filepath)?;
 
-    let contents = fs::read_to_string(filepath)
-        .expect("Should have been a pokemon name with lowercase");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+ 
+    Ok(())
+}
 
-    println!("::{}", contents);
+struct Config {
+    query: String,
+    pokemon: String,
+    filepath: String
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Self, &str> {
+        let (query, pokemon) = match args.len() {
+            3 => {
+                (args[1].clone(), args[2].clone())
+            },
+            _ => { return Err("Wrong number of arguments"); }
+        };
+        Ok(Config { pokemon: pokemon.clone(), query: query, filepath: format!("entries/{}.txt", pokemon) })
+    }
 }
