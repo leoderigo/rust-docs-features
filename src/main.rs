31b75051ce20aa8429d2c400ccbfc869ef2b9cfe
leoderigo <leoderigo.sjb@gmail.com>
2026-09-17 11:51:52 -0300

    Reading some args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let search_for = &args[1];
+    let filepath = &args[2];
+
+    println!("We are searching for {} in {}", search_for, filepath);
 }
